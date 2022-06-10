150251684742c37e6681a1c87d549553da002596
happer64bit <96770798+happer64bit@users.noreply.github.com>
2022-06-10 03:19:51 -0700

    Update build-zip.py
    Fixed Source Builder

--- script/build-zip.py
+++ script/build-zip.py
@@ -1,4 +1,8 @@
 import shutil, os
 
+os.system("mkdir bundle")
+os.system("npm install")
+os.system("parcel build src/index.html --dist-dir bundle")
+
 shutil.make_archive("build/CleanTab", 'zip', "bundle/")
 print("Builded to build/ folder")
