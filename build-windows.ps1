382994bade9491f535125a8d42b42c251829f056
Happer World <happer64bit@gmail.com>
2022-08-08 22:35:56 +0630

    fixed Copy-Item didn't copy icon

--- build-windows.ps1
+++ build-windows.ps1
@@ -3,5 +3,8 @@ npm run build                               # bundle source code
 Copy-Item res/ bundle/                      # Copy res folder to bundle folder
 
 Copy-Item .\script\manifest.json bundle\
+Copy-Item .\res\icon_128.png .\bundle\res
 
-echo "Successfully Build to bundle folder"
+Remove-Item bundle/*.map
+
+Write-Output "Successfully compiled to bundle folder"
